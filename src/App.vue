5e48acb88859cee8c4b91dd328006553dfe55766
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-04-02 02:57:51 -0500

    [#156424328] Agregando tracks como constanete 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    p
-      input(v-model="name")
-      button(v-on:click="format") Format
-    p {{ formattedName }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar cancion...",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(v-on:click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="track in tracks") {{track.name}} {{track.artist}}
+
 </template>
 
 <script>
+const TRACKS = [
+  {name: 'I don\'t want a miss a thing', artist: 'Aerosmith'},
+  {name: 'Tomorrow', artist: 'Cranberrires'},
+  {name: 'Chispa adecuada', artist: 'Heroes del silencio'},
+  {name: 'Luz de día', artist: 'Enanitos verdes'},
+  {name: 'Scar Tissue', artist: 'Red hot chilli pepers'}
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      name: '',
-      formattedName: ''
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Canciones encontradas: ${this.tracks.length}`
     }
   },
 
   methods: {
-    format () {
-      this.formattedName = this.name.split(' ').join('-').toUpperCase()
+    search () {
+      this.tracks = TRACKS
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+  .results{
+    margin-top: 50px
+  }
 </style>
