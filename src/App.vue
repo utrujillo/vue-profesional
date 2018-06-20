656e49e017eebaa5778781641443b682a7b9c209
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-06-20 11:42:24 -0500

    [#158496656] Pasando información de mi componente padre a el hijo 	modified:   src/App.vue 	new file:   src/components/Track.vue 	new file:   src/components/shared/Loader.vue 	modified:   webpack.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,53 @@
 <template lang="pug">
   #app
     pm-header
+    
+    pm-loader(v-show="isLoading")
 
-    section.section
+    section.section(v-show="!isLoading")
       nav.nav.has-shadow
         .container
-          input.input.is-large(
-            type="text",
-            placeholder="Buscar cancion...",
-            v-model="searchQuery"
-          )
-          a.button.is-info.is-large(v-on:click="search") Buscar
-          a.button.is-danger.is-large &times;
-
+          .field.has-addons
+            .control.is-expanded
+              input.input.is-large(
+                type="text",
+                placeholder="Buscar cancion...",
+                v-model="searchQuery"
+              )
+            .control
+              a.button.is-info.is-large(v-on:click="search") Buscar
+              a.button.is-danger.is-large &times;
+      
       .container
         p
           small {{ searchMessage }}
 
       .container.results
-        .columns
-          .column(v-for="track in tracks")
-            | {{track.name}} {{track.artists[0].name}}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="track in tracks")
+            pm-track(v-bind:track="track")
 
     pm-footer
 </template>
 
 <script>
-import tarckService from './services/track.js'
-import PmFooter from './components/layout/Footer.vue'
-import PmHeader from './components/layout/Header.vue'
+import tarckService from '@/services/track.js'
+import PmFooter from '@/components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false
     }
   },
 
-  components: { PmFooter, PmHeader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader },
 
   computed: {
     searchMessage () {
@@ -50,9 +58,11 @@ export default {
   methods: {
     search () {
       if (!this.searchQuery) { return }
+      this.isLoading = true
       tarckService.search(this.searchQuery)
         .then(res => {
           this.tracks = res.tracks.items
+          this.isLoading = false
         })
     }
   }
---
+++ src/components/Track.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .card
+    .card-image
+      figure.image.is-1by1
+        img(v-bind:src="track.album.images[0].url")
+
+    .card-content
+      .media
+        .media-left
+          figure.image.is-48x48
+            img(v-bind:src="track.album.images[0].url")
+        .media-content
+          p.title.is-6
+            strong {{ track.name }}
+          p.subtitle.is-6 {{ track.artists[0].name }}
+      .content
+        small {{ track.duration_ms }}
+          nav.level
+            .level-left
+              a.level-item.has-text-centered
+                span.icon.is-small ♫
+</template>
+
+<script>
+  export default {
+    props: {
+      track: { type: Object, required: true }
+    }
+  }
+</script>
