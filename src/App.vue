39e69fbcdf3a814822b53c0606335a55db6c8aee
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-06-21 10:16:18 -0500

    [#158528504] Slots en Vue 	modified:   src/App.vue 	new file:   src/components/shared/Notification.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   #app
     pm-header
-    
+    pm-notification(v-show="showNotification")
+      p(slot="body") No se encontraron resultados
+
     pm-loader(v-show="isLoading")
 
     section.section(v-show="!isLoading")
@@ -40,6 +42,7 @@ import PmFooter from '@/components/layout/Footer.vue'
 import PmHeader from '@/components/layout/Header.vue'
 import PmTrack from '@/components/Track.vue'
 import PmLoader from '@/components/shared/Loader.vue'
+import PmNotification from '@/components/shared/Notification.vue'
 
 export default {
   name: 'app',
@@ -48,11 +51,12 @@ export default {
       searchQuery: '',
       tracks: [],
       isLoading: false,
+      showNotification: false,
       selectedTrack: ''
     }
   },
 
-  components: { PmFooter, PmHeader, PmTrack, PmLoader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader, PmNotification },
 
   computed: {
     searchMessage () {
@@ -60,12 +64,24 @@ export default {
     }
   },
 
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
+
   methods: {
     search () {
       if (!this.searchQuery) { return }
       this.isLoading = true
+      this.showNotification = false
       tarckService.search(this.searchQuery)
         .then(res => {
+          this.showNotification = res.tracks.total === 0
           this.tracks = res.tracks.items
           this.isLoading = false
         })
