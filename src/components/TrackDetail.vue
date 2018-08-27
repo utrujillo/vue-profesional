51b14388979aff3857dec9c77753873091c86674
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-08-27 14:02:02 -0500

    [#159631087] Mixins - Sirve para reutilizar codigo de js, como los componentes pero en el js 	modified:   src/components/Track.vue 	modified:   src/components/TrackDetail.vue 	new file:   src/mixins/track.js

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,16 +1,37 @@
 <template lang="pug">
   .container
     .columns
-      .column.is-5.is-offset-4
-        pm-track(:track="track")
+      .column.is-3.has-text-centered
+        figure.media-left
+          p.image
+            img(v-bind:src="track.album.images[0].url")
+          p
+            a.button.is-primary.is-large
+              span.icon(v-on:click="selectTrack") &#9658;
+      .column.is-8
+        .panel
+          .panel-heading
+            h1.title {{ track.name }}
+          .panel-block
+            article.media
+              .media-content
+                .content
+                  ul(v-for="(value,key) in track")
+                    li
+                      strong {{ key }}:&nbsp;
+                      span {{ value }}
+              nav.level
+                .level-left
+                  a.level-item
+
 </template>
 
 <script>
+  import trackMixin from '@/mixins/track.js'
   import trackService from '@/services/track.js'
-  import PmTrack from '@/components/Track.vue'
 
   export default {
-    components: { PmTrack },
+    mixins: [ trackMixin ],
     data () {
       return {
         track: {}
